818047eb568525329d1a9c1421a41d60f30eed30
Kevin Patto <kevp2010@gmail.com>
2023-10-18 15:28:42 -0500

    finished inquirer user input section

---
+++ index.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            message: 'Enter up to three letters for your logo: ',
+            name: 'text',
+        },
+        {
+            type: 'input',
+            message: 'Enter a color for your text (by name OR hexadecimal): ',
+            name: 'textColor',
+        },
+        {
+            type: 'list',
+            message: 'Select a shape: ',
+            choices: ['circle', 'triangle', 'square'],
+            name: 'shape',
+        },
+        {
+            type: 'input',
+            message: 'Enter a color for your shape (by name OR hexadecimal): ',
+            name: 'shapeColor',
+        },
+    ])
+    .then((response) => {
+        const { text, textColor, shape, shapeColor } = response;
+    })
+    .catch ((err) => console.log(err));
